cb8545169d643bb245c17747d5dc59704206a1f0
Vadym Shevchenko <spawnkiev@gmail.com>
2016-01-16 22:58:49 +0200

    change tests, add parameterizing test, image absolute path in the project, constants and fix a lot of bugs

--- src/test/java/net/invest/ButtonDownload.java
+++ src/test/java/net/invest/ButtonDownload.java
@@ -3,6 +3,8 @@ package net.invest;
 import net.invest.pages.HomePage;
 import net.invest.pages.LoginPage;
 import net.invest.pages.ProfilePage;
+import net.invest.utility.Common_methods;
+import net.invest.utility.Constant;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
@@ -10,6 +12,8 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 /**
  * Created by Vadym on 1/15/2016.
@@ -22,7 +26,6 @@ public class ButtonDownload {
     private HomePage homePage;
     private ProfilePage profilePage;
     private WebDriver driver;
-    private final String URL_WEB = "https://www.4invest.net/";
 
     @DataProvider(name = "signIn", parallel = false)
     public static Object[][] testDataLogin() {
@@ -35,19 +38,17 @@ public class ButtonDownload {
         };
     }
 
-    @DataProvider(name = "pathImage", parallel = false)
-    public static Object[][] testDataPathImage() {
-        return new Object[][]{
-                new Object[]{
-                        "http://www.brunningonline.net/simon/blog/archives/South%20Park%20Avatar.jpg"
-                }
-        };
+    @DataProvider(name = "pathFile", parallel = false)
+    public static Object[][] testDataPathFile() {
+        return new Object[][] {
+                {"resources/image1.jpg", true},
+                {"resources/testFile.txt", false}};
     }
 
     @BeforeSuite
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
-        driver.get(URL_WEB);
+        driver.get(Constant.URL_WEB);
         driver.manage().window().maximize();
         loginPage = new LoginPage(driver);
         homePage = new HomePage(driver);
@@ -60,27 +61,25 @@ public class ButtonDownload {
         Assert.assertTrue(homePage.getNameProfile().equals(nameAccount));
     }
 
-    @Test(dataProvider = "pathImage", dependsOnMethods = {"logInOnWebSite"})
-    public void checkButtonDownloadAppearAfterChooseAvatar(String pathToImage){
+    @Test(dependsOnMethods = {"logInOnWebSite"})
+    public void openProfilePage(){
         homePage.openProfilePage();
-        profilePage.chooseFileUploadAvatar(pathToImage);
-        Assert.assertTrue(profilePage.isButtonDownloadPresent());
+        Assert.assertTrue(Common_methods.isElementPresent(driver, ProfilePage.CHOOSE_PHOTO_BUTTON));
     }
 
-    @Test(dependsOnMethods = {"logInOnWebSite"})
+    @Test(dataProvider = "pathFile", dependsOnMethods = {"openProfilePage"})
+    public void checkButtonDownloadAppearAfterChooseFile(String pathToFileInProject, boolean expectedResult){
+        driver.navigate().refresh();
+        String absolutePathToFile = new File(pathToFileInProject).getAbsoluteFile().toString();
+        profilePage.chooseFileUploadAvatar(absolutePathToFile);
+        Assert.assertEquals(profilePage.isButtonDownloadPresent(), expectedResult);
+    }
+
+    @Test
     public void negativeTestJustOpenAndCloseFileUploadAndCheckButtonDownload(){
         driver.navigate().refresh();
         profilePage.clickChoosePhotoButton();
         profilePage.closeFileUploadWindow();
-        Assert.assertFalse(profilePage.isButtonDownloadPresent());
-    }
-
-    @Test(dependsOnMethods = {"logInOnWebSite"})
-    public void negativeTestUploadNotAPicture(){
-        driver.navigate().refresh();
-        // WARNING: Change line below on the different computers! This path to local file!
-        String pathToTxtFile = "C:\\testFile.txt";
-        profilePage.chooseFileUploadAvatar(pathToTxtFile);
         Assert.assertFalse(profilePage.isButtonDownloadPresent());
     }
 
